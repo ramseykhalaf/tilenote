3d362f4ac3dc7194c5614ef7071602b0391c4cd4
Ramsey Khalaf <ramseykhalaf@gmail.com>
2013-09-08 18:38:59 +0400

    added floor_main template to use as a route

--- client/helpers/router.js
+++ client/helpers/router.js
@@ -1,5 +1,13 @@
 Meteor.Router.add({
+	'/floor': {
+		to: 'floor_main',
+		as: 'showFloor',
+		and: function() {
+			Session.set('floorId', null);
+		}
+	},
     '/floor/:_id': {
+    	to: 'floor_main',
         as: 'showFloor',
         and: function(_id) {
             Session.set('floorId', _id);
