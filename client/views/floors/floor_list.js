1920924d5a2584d2f93c891b044b013d5a3582ba
Ramsey Khalaf <ramseykhalaf@gmail.com>
2013-09-23 00:53:37 +0300

    clean up to templates

--- client/views/floors/floor_list.js
+++ client/views/floors/floor_list.js
@@ -1,6 +1,6 @@
 Template.floor_list.helpers({
     floorsPublic: function() {
-        return Floors.find({isPrivate: false}, {sort: {createdAt: -1}});
+        return Floors.find({isPrivate: false, ownerId: {$ne: Meteor.userId()}}, {sort: {createdAt: -1}});
     },
     floorsPrivate: function() {
     	return Meteor.userId() ? Floors.find({ownerId: Meteor.userId()} , {sort: {createdAt: -1}})
--- client/views/floors/floor_list_item.js
+++ client/views/floors/floor_list_item.js
@@ -1,12 +1,6 @@
 Template.floor_list_item.helpers({
 	currentFloor: function() {
 		return this._id === Session.get('floorId');
-	},
-	ownFloor: function() {
-		return this.ownerId === Meteor.userId()
-	},
-	tileCount: function() {
-		return Tiles.find().count();
 	}
 });
 
